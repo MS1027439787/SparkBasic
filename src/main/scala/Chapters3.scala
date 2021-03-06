5d98eb460b5ece03b27f9a47fc42a2a9bdddd6af
Rain <1027439787@qq.com>
2021-03-06 22:04:57 +0800

    函数

--- src/main/scala/Chapters3.scala
+++ src/main/scala/Chapters3.scala
@@ -15,35 +15,23 @@ object Chapters3 {
     // 它内部会构建 DAGScheduler和 TaskScheduler 对象
     val sc = new SparkContext(sparkConf)
     val rdd_list = sc.parallelize(List(1, 2, 3, 4, 5))
-    val rdd_array = sc.parallelize(Array("hadoop", "hive", "spark"))
+    val rdd_array = sc.parallelize(Array("hadoop", "hadoop", "hive", "spark"))
 
     //map自定义函数
     val rdd_list_map1 = rdd_list.map((x: Int) => x * 10)
     //等价函数
     val rdd_list_map2 = rdd_list.map(x => x * 10) //省略参数值
-    val list:Array[Int] = rdd_list_map1.collect()
-    for(i <- list) {
-      println(i)
-    }
+    rdd_list_map1.collect().foreach(println)
     val rdd_list_map3 = rdd_list.map(_ * 10) //最简写
 
     //flatMap 调用元素必须可迭代
     val rdd_array_flat_map1 = rdd_array.flatMap(x => x.tail)
-    val array1:Array[Char] = rdd_array_flat_map1.collect()
-    for(i <- array1) {
-      println(i)
-    }
+    rdd_array_flat_map1.collect().foreach(println)
     val rdd_array_flat_map2 = rdd_array.flatMap(x => x.toUpperCase)
-    val array2:Array[Char] = rdd_array_flat_map2.collect()
-    for(i <- array2) {
-      println(i)
-    }
+    rdd_array_flat_map2.collect().foreach(println)
     //mapPartitions
     val rdd_array_flat_par = rdd_array.mapPartitions(x => x.filter(_== "hadoop"))
-    val partition = rdd_array_flat_par.collect()
-    for(i <- partition) {
-      println(i)
-    }
+    rdd_array_flat_par.collect().foreach(println)
     //glom
     val rdd_array_glom = rdd_list.glom()
     //union
@@ -54,6 +42,19 @@ object Chapters3 {
     for(i <- union1) {
       println(i)
     }
+    //reduceByKey
+    val pairRdd = rdd_array.map((_,1))//rdd_array.map(x => (x,1))的简写，将x转换为键值对的形式
+    val pairRdd1 = rdd_array.map(x => (x,x.length))
+    pairRdd.collect().foreach(println)
+    pairRdd.reduceByKey(_+_).collect.foreach(println)
+    //等价于下面写法(reduceByKey会寻找相同key的数据，当找到这样的两条记录时会对其value(分别记为x,y)做(x,y) => x+y的处理，即只保留求和之后的数据作为value)
+    pairRdd1.reduceByKey((x,y) => x + y).collect.foreach(println)
+    //groupByKey
+    pairRdd.groupByKey().foreach(println)
+    pairRdd.groupByKey().map(t => (t._1,t._2.sum)).foreach(println)
+    //combineByKey
+    pairRdd.combineByKey((v : Int) => List(v), (c : List[Int], v : Int) => v :: c, (c1 : List[Int], c2 : List[Int]) => c1 ::: c2).collect.foreach(println)
+    //val wordcount = rdd.flatMap(_.split(' ')).map((_,1)).reduceByKey(_+_).map(x => (x._2,x._1)).sortByKey(false).map(x => (x._2,x._1)).saveAsTextFile("/data/project/result2")
   }
 
   def dirDel(path: File) {
--- src/test/Test.scala
+++ src/test/Test.scala
@@ -5,14 +5,50 @@ object Test{
   import java.io.PrintWriter
 
   def main(args:Array[String]):Unit={
+
+
     //本地设置
     System.setProperty("hadoop.home.dir","E:\\hadoop-3.0.1")
     //1、构建sparkConf对象 设置application名称和master地址
     //本地
-    val sparkConf:SparkConf=new SparkConf().setAppName("WordCount").setMaster("local[2]")
+    val sparkConf:SparkConf=new SparkConf().setAppName("WordCount").setMaster("local")
     //2、构建sparkContext对象,该对象非常重要，它是所有spark程序的执行入口
     // 它内部会构建 DAGScheduler和 TaskScheduler 对象
     val sc=new SparkContext(sparkConf)
+
+
+    val x = sc.parallelize(List("spark", "rdd", "example",  "sample", "example"), 3)
+    val y1 = x.map(x => (x, 1))
+    y1.collect
+    // res0: Array[(String, Int)] =
+    //    Array((spark,1), (rdd,1), (example,1), (sample,1), (example,1))
+    // rdd y can be re writen with shorter syntax in scala as
+    val y2 = x.map((_, 1))
+    y2.collect
+    // res1: Array[(String, Int)] =
+    //    Array((spark,1), (rdd,1), (example,1), (sample,1), (example,1))
+    // Another example of making tuple with string and it's length
+    val y3 = x.map(x => (x, x.length))
+    y3.collect
+    // res3: Array[(String, Int)] =
+    //    Array((spark,5), (rdd,3), (example,7), (sample,6), (example,7))
+
+    //reduceByKey
+    //reduceByKey会寻找相同key的数据，当找到这样的两条记录时会对其value(分别记为x,y)做(x,y) => x+y的处理，即只保留求和之后的数据作为value。反复执行这个操作直至每个key只留下一条记录。
+    val word = y1.reduceByKey((x,y) => x+y)
+    val word1 = y1.reduceByKey(_+_)
+
+    /**
+     * 通过groupByKey()后调用map遍历每个分组，然后通过t => (t._1,t._2.sum)对每个分组的值进行累加。
+     * 因为groupByKey()操作是把具有相同类型的key收集到一起聚合成一个集合，集合中有个sum方法，对所有元素进行求和。
+     * 注意，（k，v）形式的数据，我们可以通过 ._1，._2 来访问键和值，
+     * 用占位符表示就是 _._1，_._2，这里前面的两个下划线的含义是不同的，前边下划线是占位符，后边的是访问方式。
+     * 我们记不记得 ._1，._2，._3 是元组的访问方式。我们可以把键值看成二维的元组。
+     */
+    y1.reduceByKey(_+_).collect.foreach(println)
+    //等价于
+    y1.groupByKey().map(t => (t._1,t._2.sum)).collect.foreach(println)
+
     val rdd1=sc.parallelize(List(1,2,3,4,5,6,7,8,9))
     //每个元素乘10，_指集合每个元素
     val list2=rdd1.map(_*10).collect
