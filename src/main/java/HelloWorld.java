7e8792cf86aeb16c5830b5c2c537859cbab5d4a8
masai <masai@95303.com>
2021-03-11 17:37:15 +0800

    sparkStreaming

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,7 @@
 import org.codehaus.jackson.map.util.ISO8601Utils;
 
+import java.io.IOException;
+
 /**
  * @author masai
  * @date 2021/3/5
@@ -7,5 +9,12 @@ import org.codehaus.jackson.map.util.ISO8601Utils;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println(0.3-0.2);
+        RuntimeException e = new RuntimeException();
+        String tmp = "abcaaaaaaa";
+        String tmp1 = tmp.replace('a','b');
+        CharSequence abc = "abc";
+        String tmp2 = tmp1.replaceAll("bb","w");
+        System.out.println(tmp1);
+        System.out.println(tmp2);
     }
 }
