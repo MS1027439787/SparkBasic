a12c856242e2bca0f18224a700a1347c02ac3574
masai <1027439787@qq.com>
2021-10-19 17:23:00 +0800

    20211019

--- src/main/scala/basicPractice/WordCount.scala
+++ src/main/scala/basicPractice/WordCount.scala
@@ -22,10 +22,36 @@ object WordCount {
     //3、读取数据文件Starry Starry Night
     //val data: RDD[String] = sc.textFile("/tmp/daoshu/tmp_wcl_dunj_sevn_zx.csv")
     val data: RDD[String] = sc.textFile("./src/main/resources/words")
+
+    val dataResult:Array[String] = data.collect()
+    for (i <- dataResult) {
+      println(i)
+    }
     //4、 切分每一行，获取所有单词，空格也会被算作一个字符
     val words: RDD[String] = data.flatMap(x => x.split(" "))
+    val wordsResult:Array[String] = words.collect()
+    for (i <- wordsResult) {
+      println(i)
+    }
+    //测试方法
+    val wordsmap: RDD[Array[String]] = data.map(x => x.split(" "))
+    val wordsmapResult:Array[Array[String]] = wordsmap.collect()
+    for (i <- wordsmapResult) {
+      println(i)
+    }
     //5、每个单词计为1
     val wordAndOne: RDD[(String, Int)] = words.map((_, 1))
+
+    //测试方法ª
+//    “flatMap “函数的一半功能和map函数一样，不过有个要求，传入的函数在处理完后返回值必须是List(应该是Seq)，如果结果不是List(Seq)，
+//    那么将出错。也就是说，传入的函数是有要求的——返回值是Seq才行。这样，每个元素处理后返回一个List，
+//    我们得到一个包含List元素的List，flatMap自动将所有的内部list的元素取出来构成一个List返回。
+//    val wordAndOneflatmap: RDD[Array[Tuple2[String, Int]]] = words.flatMap((_, 1))
+    val wordAndOneResult: Array[(String, Int)] = wordAndOne.collect()
+
+    for (i <- wordAndOneResult) {
+      println(i)
+    }
     //6、相同单词出现的1累加
     val result: RDD[(String, Int)] = wordAndOne.reduceByKey(_ + _)
     //按照单词出现的次数降序排列 第二个参数默认是true表示升序，设置为false表示降序
